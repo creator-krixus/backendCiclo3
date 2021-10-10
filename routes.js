d18aab270b0faef5bfe1529429bb7b5b98e625f8
creator-krixus <wilsonrueda2@gmail.com>
2021-10-10 13:11:47 -0500

    feature: Add function delete and edit usuario

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = express.Router()
 
+//Mostrar todos los usuarios
 routes.get('/', (req, res) => {
     req.getConnection((err, conn) => {
         if(err){
@@ -17,6 +18,7 @@ routes.get('/', (req, res) => {
     })
 })
 
+//Insertar nuevos usuarios
 routes.post('/', (req, res) => {
     req.getConnection((err, conn) => {
         if(err){
@@ -27,6 +29,40 @@ routes.post('/', (req, res) => {
                     return res.send(err);
                 }else{
                      res.send('Se registro el usuario');
+                }
+            })
+        }
+    })
+})
+
+//Borrar un usuario buscandolo por el id
+routes.delete('/:id', (req, res) => {
+    req.getConnection((err, conn) => {
+        if(err){
+            return res.send(err);
+        }else{
+            conn.query('DELETE FROM usuarios WHERE id = ?', [req.params.id] ,(err, rows) => {
+                if(err){
+                    return res.send(err);
+                }else{
+                     res.send('Usuario borrado');
+                }
+            })
+        }
+    })
+})
+
+//Actualizar un usuario buscandolo por el id
+routes.post('/:id', (req, res) => {
+    req.getConnection((err, conn) => {
+        if(err){
+            return res.send(err);
+        }else{
+            conn.query('UPDATE usuarios set ? WHERE id = ?', [req.body, req.params.id] ,(err, rows) => {
+                if(err){
+                    return res.send(err);
+                }else{
+                     res.send('Usuario actualizado');
                 }
             })
         }
